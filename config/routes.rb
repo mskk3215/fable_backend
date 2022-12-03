f0360bbe26a21780b86956a2b5f22ccc039dbf6b
mskk3215 <m.kaaiweb@gmail.com>
2022-12-04 01:32:00 +0900

    fix: sign up api

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post "/signup", to: "registrations#create"
-
+      # resources :registrations, only: [:index]
+      resources :registrations, only: [:index, :create]
+      # post "/signup", to: "registrations#signup"
     end
   end
 end
