ba2accf74df0272f22a422a77d3b3890b627f3b0
mskk3215 <m.kaaiweb@gmail.com>
2022-12-15 23:40:15 +0900

    fix:cookie

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module FableBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
+    # config.session_store :cookie_store
+    # config.middleware.insert_before ActiveRecord::Middleware::DatabaseSelector, ActionDispatch::Cookies
+    # config.middleware.insert_before ActiveRecord::Middleware::DatabaseSelector, ActionDispatch::Session::CookieStore
   end
 end
