9f9950bb69d798658e2cde512e2f6ddf4b078c84
mskk3215 <m.kaaiweb@gmail.com>
2023-01-01 12:20:40 +0900

    fix:CarrierWave path setting

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,10 +7,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  def asset_host
+    return "http://localhost:3001"
+  end
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+¥    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
