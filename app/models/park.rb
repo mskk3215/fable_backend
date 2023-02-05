9d311784a25a944bdf301359995f53ab79536d97
mskk3215 <m.kaaiweb@gmail.com>
2023-02-05 23:24:01 +0900

    parks routing&controller&model&migration add

---
+++ app/models/park.rb
@@ -0,0 +1,13 @@
+class Park < ApplicationRecord
+  
+    VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+
+    validates :name, presence: true
+    validates :post_code, presence: true, format: {with: VALID_POSTAL_CODE_REGEX}
+    validates :address, presence: true
+    validates :latitude, presence: true
+    validates :longitude, presence: true
+  
+    has_many :posts
+
+end
