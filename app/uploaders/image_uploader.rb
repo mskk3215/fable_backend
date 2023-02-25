559940611bc306c074616540dee8e9b9d1bb2df9
mskk3215 <m.kaaiweb@gmail.com>
2023-02-25 17:28:51 +0900

    upload image size modify

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -28,7 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [200, 200]
+  process resize_to_fit: [300, 300]
   #
   # def scale(width, height)
   #   # do something
@@ -36,7 +36,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :post do
-    process resize_to_fit: [200, 200]
+    process resize_to_fit: [300, 300]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
