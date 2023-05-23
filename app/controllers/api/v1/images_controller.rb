5ed5e21c99df4d978833d9e8b3e6c3bcafcbefdc
mskk3215 <m.kaaiweb@gmail.com>
2023-05-23 23:25:41 +0900

    bulk_update action modified in “Image controller"
    [Feature Addition] Added taken_at field
    [Feature Modification] Modified to allow saving without insect, park, and city fields
    [Feature Modification] If the city is updated, the park will be removed
    [Feature Modification] Add a new record to the database if the park name does not exist

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -24,9 +24,19 @@ module Api
       def bulk_update
         ActiveRecord::Base.transaction do
           @images.each do |image|
-            insect = Insect.find_by!(name: image_params[:name].split(','), sex: image_params[:sex].split(','))
-            city = City.find_by!(name: image_params[:cityName])
-            image.update!(insect_id: insect.id, park_id: image_params[:park_id], city_id: city.id, taken_at: image_params[:taken_at])
+            insect = Insect.find_by(name: image_params[:name].split(','), sex: image_params[:sex].split(','))
+            city = City.find_by(name: image_params[:cityName])
+            park = find_or_create_park(image_params[:parkName], city)
+
+            insect_id = insect&.id || image.insect_id
+            park_id = park&.id || image.park_id
+            city_id = city&.id || image.city_id
+            taken_at = image_params[:taken_at].presence || image.taken_at
+
+            # cityがあってparkがない場合はpark_idを削除する
+            park_id = nil if city.present? && park.blank?
+
+            image.update!(insect_id:, park_id:, city_id:, taken_at:)
           end
         end
       rescue ActiveRecord::RecordInvalid
@@ -45,11 +55,24 @@ module Api
       private
 
       def image_params
-        params.require(:image).permit({ image: [] }, :name, :sex, :park_id,:cityName, :taken_at)
+        params.require(:image).permit({ image: [] }, :name, :sex, :parkName, :cityName, :taken_at)
       end
 
       def set_image
         @images = Image.find(params[:id].split(','))
+      end
+
+      def find_or_create_park(park_name, city)
+        # 公園名も市町村名もない場合と、公園名がなく市町村名だけの場合はnilを返す
+        return nil if park_name.blank? || city.blank?
+
+        # 公園名がDBにある場合、その公園名を返す
+        park = Park.find_by(name: park_name, city_id: city.id)
+        # 公園名がDBにない場合、新規でDBに公園名を登録する
+        if park_name.present? && park.blank? && city.present?
+          park = Park.create!(name: park_name, city_id: city.id, prefecture_id: city.prefecture_id)
+        end
+        park
       end
     end
   end
