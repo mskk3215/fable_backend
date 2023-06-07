2376f92efd186e0a49705fc3c8503b08bbab6e06
mskk3215 <m.kaaiweb@gmail.com>
2023-06-08 00:17:50 +0900

    "Image" test add in Rspec

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    @image = FactoryBot.build(:image, image_path: 'public/images/test_image.png', user_id: user.id)
+  end
+
+  describe do
+    context '投稿できる場合' do
+      it 'imageがあれば投稿できる' do
+        expect(@image).to be_valid
+      end
+    end
+
+    context '投稿できない場合' do
+      it 'imageが空では投稿できない' do
+        @image.image = nil
+        @image.valid?
+        expect(@image.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '紐づくユーザーが存在しないと投稿できない' do
+        @image.user = nil
+        @image.valid?
+        expect(@image.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
+end
