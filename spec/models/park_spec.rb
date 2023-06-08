15edcb72e6bfe25b9da4d84384e4c4c2ad31bbc7
mskk3215 <m.kaaiweb@gmail.com>
2023-06-09 00:23:14 +0900

    "Park" test add in Rspec

---
+++ spec/models/park_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Park, type: :model do
+  before do
+    @park = FactoryBot.create(:park)
+  end
+
+  describe do
+    context 'parkを保存できる場合' do
+      it 'name,city_id,prefecture_idがあれば保存できる' do
+        expect(@park).to be_valid
+      end
+    end
+
+    context 'parkを保存できない場合' do
+      it 'nameが空では保存できない' do
+        @park.name = nil
+        @park.valid?
+        expect(@park.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'city_idが空では保存できない' do
+        @park.city_id = nil
+        @park.valid?
+        expect(@park.errors.full_messages).to include("City can't be blank")
+      end
+      it 'prefecture_idが空では保存できない' do
+        @park.prefecture_id = nil
+        @park.valid?
+        expect(@park.errors.full_messages).to include("Prefecture can't be blank")
+      end
+    end
+  end
+end
