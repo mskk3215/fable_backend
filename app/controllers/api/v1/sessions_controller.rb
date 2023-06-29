a22651b5599879bf15cc5e48cf0cf087fb8cdb50
mskk3215 <m.kaaiweb@gmail.com>
2023-06-30 00:21:57 +0900

    fix:error message for login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,7 +14,7 @@ module Api
           render json: { logged_in: true, user: @user }
 
         else
-          render json: { status: 401, errors: ['認証に失敗しました', '正しいメールアドレス・パスワードを入力し直すか、新規登録を行ってください。'] }
+          render json: { errors: ['ログインに失敗しました。', '入力した情報を確認して再度お試しください。'] } ,status: :unauthorized
         end
       end
 
