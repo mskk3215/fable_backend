4ded5787b45ec12f597db4ed0833129dab2b629c
mskk3215 <m.kaaiweb@gmail.com>
2023-08-01 23:58:51 +0900

    feat: avatar uploader add

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  mount_uploader :avatar, AvatarUploader
 
   validates :nickname, presence: true, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
