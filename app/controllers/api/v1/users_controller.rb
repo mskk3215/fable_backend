35082424f52f5b684c06b562b1b797ddd056394f
mskk3215 <m.kaaiweb@gmail.com>
2023-08-04 22:47:56 +0900

    feat:update action add to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,10 +15,48 @@ module Api
           render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
         end
       end
+
+      def update
+        @user = User.find(params[:id])
+        if users_params[:new_password].present?
+          handle_password_update
+        else
+          handle_profile_update
+        end
+      end
+
       private
 
       def users_params
         params.require(:user).permit(:nickname, :email, :password, :new_password, :avatar)
+      end
+
+      def handle_password_update
+        @user = User.find(params[:id])
+        if @user.authenticate(users_params[:password])
+          if @user.update!(
+            password: users_params[:new_password]
+          )
+            render 'api/v1/users/update'
+          else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { errors: ['現在のパスワードが間違っています'] }, status: :unprocessable_entity
+        end
+      end
+
+      def handle_profile_update
+        if User.exists?(nickname: users_params[:nickname]) && @user.nickname != users_params[:nickname]
+          render json: { errors: ['そのニックネームは既に使用されています'] }, status: :unprocessable_entity
+        elsif User.exists?(email: users_params[:email]) && @user.email != users_params[:email]
+          render json: { errors: ['そのメールアドレスは既に使用されています'] }, status: :unprocessable_entity
+        elsif @user.update!(nickname: users_params[:nickname], email: users_params[:email],
+                            avatar: users_params[:avatar].presence || @user.avatar)
+          render 'api/v1/users/update'
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,    presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 
   has_many  :images
 end
