99ae85ae43db51cb5f8202fab3950e9c41f7220a
mskk3215 <m.kaaiweb@gmail.com>
2023-08-07 21:41:16 +0900

    feat:relationships migration add

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_321_105_830) do
+ActiveRecord::Schema[7.0].define(version: 20_230_805_145_353) do
   create_table 'cities', charset: 'utf8mb3', force: :cascade do |t|
     t.string 'name', null: false
     t.bigint 'prefecture_id', null: false
@@ -70,6 +70,16 @@ ActiveRecord::Schema[7.0].define(version: 20_230_321_105_830) do
     t.string 'name', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'relationships', charset: 'utf8mb3', force: :cascade do |t|
+    t.integer 'follower_id', null: false
+    t.integer 'followed_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
+    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
+    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
   end
 
   create_table 'users', charset: 'utf8mb3', force: :cascade do |t|
