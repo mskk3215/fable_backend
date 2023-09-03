d86ac2b6b6f701244f4762ef56b04f205f199e17
mskk3215 <m.kaaiweb@gmail.com>
2023-09-03 16:40:44 +0900

    feat:relationship model test add

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,5 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'フォロー機能' do
+    before do
+      follower = FactoryBot.create(:user)
+      followed = FactoryBot.create(:user)
+      @relationship = FactoryBot.build(:relationship, follower_id: follower.id, followed_id: followed.id)
+    end
+
+    context 'フォローできる場合' do
+      it 'follower_idとfollowed_idがあればフォローできる' do
+        expect(@relationship).to be_valid
+      end
+    end
+
+    context 'フォローできない場合' do
+      it 'follower_idがないとフォローできない' do
+        @relationship.follower_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include("Follower can't be blank")
+      end
+      it 'followed_idがないとフォローできない' do
+        @relationship.followed_id = nil
+        @relationship.valid?
+        expect(@relationship.errors.full_messages).to include("Followed can't be blank")
+      end
+    end
+  end
 end
