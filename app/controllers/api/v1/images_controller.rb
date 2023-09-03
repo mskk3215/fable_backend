0bcb4a896a7dbeae7d3fad31dbe8d4aa2956a661
mskk3215 <m.kaaiweb@gmail.com>
2023-09-03 15:56:13 +0900

    refactor:images controller&api

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -6,13 +6,8 @@ module Api
       before_action :set_image, only: %i[bulk_update destroy]
 
       def index
-        @images = if params[:user_id].present?
-                    # binding.pry
-                    Image.where(user_id: params[:user_id]).order(created_at: :desc).includes(:insect, :city)
-                  else
-                    # binding.pry
-                    Image.where(user_id: current_user.id).order(created_at: :desc).includes(:insect, :city)
-                  end
+        user_id = params[:user_id].presence || current_user.id
+        @images = Image.where(user_id:).order(created_at: :desc).includes(:insect, :city)
         render 'api/v1/images/index'
       end
 
@@ -28,16 +23,15 @@ module Api
             insect = insects[image_params[:name]]
             city = cities[image_params[:cityName]]
             park = find_or_create_park(image_params[:parkName], city)
-
-            insect_id = insect&.id || image.insect_id
-            park_id = park&.id || image.park_id
-            city_id = city&.id || image.city_id
-            taken_at = image_params[:taken_at].presence || image.taken_at
-
+            attributes = {
+              insect_id: insect&.id || image.insect_id,
+              park_id: park&.id || image.park_id,
+              city_id: city&.id || image.city_id,
+              taken_at: image_params[:taken_at].presence || image.taken_at
+            }
             # cityがあってparkがない場合はpark_idを削除する
-            park_id = nil if city.present? && park.blank?
-
-            image.update!(insect_id:, park_id:, city_id:, taken_at:)
+            attributes[:park_id] = nil if city.present? && park.new_record?
+            image.update!(attributes)
           end
         end
       rescue ActiveRecord::RecordInvalid
@@ -68,10 +62,11 @@ module Api
         return nil if park_name.blank? || city.blank?
 
         # 公園名がDBにある場合、その公園名を返す
-        park = Park.find_by(name: park_name, city_id: city.id)
+        park = Park.find_or_initialize_by(name: park_name, city_id: city.id)
         # 公園名がDBにない場合、新規でDBに公園名を登録する
-        if park_name.present? && park.blank? && city.present?
-          park = Park.create!(name: park_name, city_id: city.id, prefecture_id: city.prefecture_id)
+        if park.new_record?
+          park.prefecture_id = city.prefecture_id
+          park.save!
         end
         park
       end
