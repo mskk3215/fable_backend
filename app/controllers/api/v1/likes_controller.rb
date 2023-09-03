5a1a7201413cbe9b3dc94dba19f22c585c9cb827
mskk3215 <m.kaaiweb@gmail.com>
2023-09-03 16:08:52 +0900

    fix:error handling of "likes"&"relationships"

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -5,19 +5,23 @@ module Api
     class LikesController < ApplicationController
       def create
         @like = current_user.likes.build(image_id: like_params[:image_id])
-        if @like.save!
+        if @like.save
           render 'api/v1/images/likes/create'
         else
-          render json: { error: 'Unable to like this image.' }
+          render json: { error: 'Unable to like this image.' }, status: :unprocessable_entity
         end
       end
 
       def destroy
         @like = current_user.likes.find_by(image_id: like_params[:image_id])
-        if @like.destroy
-          render 'api/v1/images/likes/destroy'
+        if @like
+          if @like.destroy
+            render 'api/v1/images/likes/destroy'
+          else
+            render json: { error: 'Unable to unlike this image.' }, status: :unprocessable_entity
+          end
         else
-          render json: { error: 'Unable to unlike this image.' }
+          render json: { error: 'Like not found' }, status: :not_found
         end
       end
 
