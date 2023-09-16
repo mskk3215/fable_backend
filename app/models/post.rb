9e2d5ef7ffb7e478c3c7555bf1bb98f0aa317faa
mskk3215 <m.kaaiweb@gmail.com>
2023-09-16 22:56:12 +0900

    refactor: postlist tab handling moved from frontend to server

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,21 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :images, dependent: :destroy
+
+  def self.fetch_all_with_includes
+    all.includes(images: %i[insect park city user
+                            likes]).order(created_at: :desc)
+  end
+
+  def self.for_followed_users(user)
+    where(user_id: user.following.pluck(:id))
+  end
+
+  def self.from_the_last_week
+    where('created_at > ?', 1.week.ago)
+  end
+
+  def self.sort_by_recent_likes
+    all.to_a.sort_by { |post| post.images.sum(&:likes_count) }.reverse
+  end
 end
