1acb3c3b0bd9ab4fcaa13e93f4520a81cdfff338
mskk3215 <m.kaaiweb@gmail.com>
2023-09-22 21:45:02 +0900

    fix:session_store to allow cookies across domains updated2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_fable_backend', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_fable_backend', domain: 'mskk3215.github.io'
