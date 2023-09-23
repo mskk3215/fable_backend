f103920e17788594e7a7c205e6aa70414b9aaa4c
mskk3215 <m.kaaiweb@gmail.com>
2023-09-23 14:17:44 +0900

    fix:session_store&cors setting modified2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_fable_backend', same_site: :none, secure: true
+Rails.application.config.session_store :cookie_store, key: '_fable_backend'
+# Rails.application.config.session_store :cookie_store, key: '_fable_backend', same_site: :none, secure: true
