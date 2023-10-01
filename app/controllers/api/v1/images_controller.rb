fcdbfa33bb4ebf2e59387c8a78e85d8405ee0f5b
mskk3215 <m.kaaiweb@gmail.com>
2023-10-01 18:48:28 +0900

    refactor:Image Create and Post Logic moved to Form Object

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -16,25 +16,12 @@ module Api
       end
 
       def bulk_update
-        ActiveRecord::Base.transaction do
-          # insect, cityの事前呼び出し
-          insect = Insect.find_by(name: image_params[:name], sex: image_params[:sex]) if image_params[:name].present?
-          city = City.find_by(name: image_params[:city_name]) if image_params[:city_name].present?
-          park = Park.find_or_create_park(image_params[:park_name], city)
-          @images.each do |image|
-            attributes = {
-              insect_id: insect&.id || image.insect_id,
-              park_id: park&.id || image.park_id,
-              city_id: city&.id || image.city_id,
-              taken_at: image_params[:taken_at].presence || image.taken_at
-            }
-            # cityがあってparkがない場合はpark_idを削除する
-            attributes[:park_id] = nil if city.present? && park.new_record?
-            image.update!(attributes)
-          end
+        form = ImageForm.new(images: @images, image_params:)
+        if form.save
+          render json: { status: :updated }
+        else
+          render json: { error_messages: '予期せぬエラーが発生しました' }, status: 500
         end
-      rescue ActiveRecord::RecordInvalid
-        render json: { error_messages: '予期せぬエラーが発生しました' }, status: 500
       end
 
       def destroy
