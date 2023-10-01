d4a5205d4d86f273b9a9f1c6832314845edd8a23
mskk3215 <m.kaaiweb@gmail.com>
2023-10-01 15:00:51 +0900

    refactor:find_or_create_park moved from Controller to Park Model

--- app/models/park.rb
+++ app/models/park.rb
@@ -11,4 +11,19 @@ class Park < ApplicationRecord
   has_many :insects, through: :insect_parks
   belongs_to :city
   belongs_to :prefecture
+
+  # 公園名と市町村名から公園を検索し、なければ新規で作成する
+  def self.find_or_create_park(park_name, city)
+  # 公園名も市町村名もない場合と、公園名がなく市町村名だけの場合はnilを返す
+    return nil if park_name.blank? || city.blank?
+
+  # 公園名がDBにある場合、その公園名を返す
+    park = Park.find_or_initialize_by(name: park_name, city_id: city.id)
+  # 公園名がDBにない場合、新規でDBに公園名を登録する
+    if park.new_record?
+      park.prefecture_id = city.prefecture_id
+      park.save!
+    end
+    park
+  end
 end
