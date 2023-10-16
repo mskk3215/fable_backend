59c8f7484d0cb16843ee3de34556f0e5c980bdf5
mskk3215 <m.kaaiweb@gmail.com>
2023-10-16 23:06:32 +0900

    fix:user mailer add

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class UserMailer < ApplicationMailer
   default from: 'noreply@fable-insect-search.com'
 
   def confirm_new_email(user)
     @user = user
-    mail(to: @user.email, subject: 'メールアドレス変更の確認')
+    mail(to: @user.email, subject: I18n.t('user_mailer.email_change_confirmation'))
   end
 
   def notify_old_email(old_email)
-    mail(to: old_email, subject: 'メールアドレスが変更されました')
+    mail(to: old_email, subject: I18n.t('user_mailer.email_change_notification'))
   end
 end
