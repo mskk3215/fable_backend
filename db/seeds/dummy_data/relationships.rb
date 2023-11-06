89ba6e4368020f1a6d3fa3ee02374e4f90971912
mskk3215 <m.kaaiweb@gmail.com>
2023-11-06 22:05:39 +0900

    feat:seeds data about biological_families, insects, likes, relationships add

---
+++ db/seeds/dummy_data/relationships.rb
@@ -0,0 +1,17 @@
+puts 'Start inserting Relationships data'
+
+users = User.all
+
+users.each do |user|
+  # 自分自身を除いたユーザーの配列を作成
+  other_users = users.reject { |u| u.id == user.id }
+  
+  # 15人以上をランダムに選択
+  followed_users = other_users.sample(rand(15..other_users.size))
+
+  followed_users.each do |followed_user|
+    Relationship.create!(follower_id: user.id, followed_id: followed_user.id)
+  end
+end
+
+puts 'Relationships data created!'
