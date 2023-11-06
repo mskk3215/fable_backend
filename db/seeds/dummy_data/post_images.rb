14fce0a32701b38e757b79500f5646c9d49724de
mskk3215 <m.kaaiweb@gmail.com>
2023-11-06 22:01:53 +0900

    feat:seeds data about post_images add

---
+++ db/seeds/dummy_data/post_images.rb
@@ -0,0 +1,75 @@
+require 'unicode'
+
+puts 'Start inserting Post_images data'
+
+image_filenames = Dir[Rails.root.join('db/seeds/dummy_data/images/*.png')].sort
+
+# parkのseedsデータをイテレーション可能な形にする
+parks = Park.all.to_a
+park_index = 0
+
+# user_id=1で全ての画像を読み込む
+user_id = 1
+image_filenames.each do |image_path|
+  # insect_idをファイル名から抽出
+  insect_id = File.basename(image_path, ".png").split("_").last.to_i
+  normalized_path = Unicode.normalize_C(image_path)
+  if normalized_path.include?("カブトムシ")
+    park = parks[park_index]
+    park_index = (park_index + 1) % parks.length 
+    park_id = park.id
+    city_id = park.city_id
+  else
+    park = parks.sample
+    park_id = park.id
+    city_id = park.city_id
+  end
+
+  # postsテーブルのデータ作成
+  post = Post.create!(user_id: user_id)
+
+  # imagesテーブルのデータ作成
+  image = Image.new(
+    image: File.open(image_path),
+    created_at: "2023-08-10 13:55:13.469244",
+    taken_at: "2023-08-10 13:55:13.469244",
+    insect_id: insect_id,
+    park_id: park_id,
+    city_id: city_id,
+    user_id: user_id,
+    post_id: post.id
+  )
+  image.save!
+end
+
+# user_id=2からuser_id=20まで、画像をランダムに10枚読み込む
+(2..20).each do |user_id|
+  # 画像をランダムにシャッフルして、最初の10枚を選択
+  random_images = image_filenames.shuffle.take(10)
+  
+  post = Post.create!(user_id: user_id)
+
+  random_images.each do |image_path|
+    # insect_idをファイル名から抽出
+    insect_id = File.basename(image_path, ".png").split("_").last.to_i
+
+    park = parks.sample
+    park_id = park.id
+    city_id = park.city_id
+
+    # imagesテーブルのデータ作成
+    image = Image.new(
+      image: File.open(image_path),
+      created_at: "2023-08-10 13:55:13.469244",
+      taken_at: "2023-08-10 13:55:13.469244",
+      insect_id: insect_id,
+      park_id: park_id,
+      city_id: city_id,
+      user_id: user_id,
+      post_id: post.id
+    )
+    image.save!
+  end
+end
+
+puts 'Post_images data created!'
