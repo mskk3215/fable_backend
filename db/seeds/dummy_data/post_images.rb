4003d5e73a2e3a184ddfd8d2cbfeec2e7d5e2c9b
mskk3215 <m.kaaiweb@gmail.com>
2023-11-15 23:54:54 +0900

    refactor:dummy_data updated

--- db/seeds/dummy_data/post_images.rb
+++ db/seeds/dummy_data/post_images.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'unicode'
 
 puts 'Start inserting Post_images data'
 
-image_filenames = Dir[Rails.root.join('db/seeds/dummy_data/images/*.png')].sort
+image_filenames = Dir[Rails.root.join('db/seeds/dummy_data/images/*.png')]
 
 # parkのseedsデータをイテレーション可能な形にする
 parks = Park.all.to_a
@@ -12,36 +14,34 @@ park_index = 0
 user_id = 1
 image_filenames.each do |image_path|
   # insect_idをファイル名から抽出
-  insect_id = File.basename(image_path, ".png").split("_").last.to_i
+  insect_id = File.basename(image_path, '.png').split('_').last.to_i
   normalized_path = Unicode.normalize_C(image_path)
-  if normalized_path.include?("カブトムシ")
+  if normalized_path.include?('カブトムシ')
     park = parks[park_index]
-    park_index = (park_index + 1) % parks.length 
-    park_id = park.id
-    city_id = park.city_id
+    park_index = (park_index + 1) % parks.length
   else
     park = parks.sample
-    park_id = park.id
-    city_id = park.city_id
   end
+  park_id = park.id
+  city_id = park.city_id
 
   # postsテーブルのデータ作成
-  post = Post.create!(user_id: user_id)
+  post = Post.create!(user_id:)
 
   # ランダムな日時を生成
-  start_date = Time.new(2023, 7, 1)
-  end_date = Time.new(2023, 8, 31)
-  random_time = Time.at((end_date.to_f - start_date.to_f) * rand + start_date.to_f)
+  start_date = Time.zone.local(2023, 7, 1)
+  end_date = Time.zone.local(2023, 8, 31)
+  random_time = Time.zone.at(((end_date.to_f - start_date.to_f) * rand) + start_date.to_f)
 
   # imagesテーブルのデータ作成
   image = Image.new(
     image: File.open(image_path),
-    created_at: "2023-09-01 13:55:13.469244",
+    created_at: '2023-09-01 13:55:13.469244',
     taken_at: random_time,
-    insect_id: insect_id,
-    park_id: park_id,
-    city_id: city_id,
-    user_id: user_id,
+    insect_id:,
+    park_id:,
+    city_id:,
+    user_id:,
     post_id: post.id
   )
   image.save!
@@ -51,31 +51,31 @@ end
 (2..20).each do |user_id|
   # 画像をランダムにシャッフルして、最初の10枚を選択
   random_images = image_filenames.shuffle.take(10)
-  
-  post = Post.create!(user_id: user_id)
+
+  post = Post.create!(user_id:)
 
   random_images.each do |image_path|
     # insect_idをファイル名から抽出
-    insect_id = File.basename(image_path, ".png").split("_").last.to_i
+    insect_id = File.basename(image_path, '.png').split('_').last.to_i
 
     park = parks.sample
     park_id = park.id
     city_id = park.city_id
 
     # ランダムな日時を生成
-    start_date = Time.new(2023, 7, 1)
-    end_date = Time.new(2023, 8, 31)    
-    random_time = Time.at((end_date.to_f - start_date.to_f) * rand + start_date.to_f)
-    
+    start_date = Time.zone.local(2023, 7, 1)
+    end_date = Time.zone.local(2023, 8, 31)
+    random_time = Time.zone.at(((end_date.to_f - start_date.to_f) * rand) + start_date.to_f)
+
     # imagesテーブルのデータ作成
     image = Image.new(
       image: File.open(image_path),
-      created_at: "2023-09-01 13:55:13.469244",
+      created_at: '2023-09-01 13:55:13.469244',
       taken_at: random_time,
-      insect_id: insect_id,
-      park_id: park_id,
-      city_id: city_id,
-      user_id: user_id,
+      insect_id:,
+      park_id:,
+      city_id:,
+      user_id:,
       post_id: post.id
     )
     image.save!
