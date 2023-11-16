a9155ae43f838bf4a22c891d5ba5d7355ef11b5b
mskk3215 <m.kaaiweb@gmail.com>
2023-11-16 22:48:50 +0900

    refactor:insects controller

--- app/controllers/api/v1/insects_controller.rb
+++ app/controllers/api/v1/insects_controller.rb
@@ -6,21 +6,80 @@ module Api
       skip_before_action :ensure_logged_in, only: %i[index]
 
       def index
-        if params[:status].present?
+        @insects = if params[:status].present?
+                     insect_park_data = initialize_insect_park_data
 
+                     case params[:status]
+                     when 'collected'
+                       fetch_collected_insects(insect_park_data)
+                     when 'uncollected'
+                       fetch_uncollected_insects(insect_park_data)
+                     end
+
+                   else
+                     fetch_all_insects
+                   end
+
+        render 'api/v1/insects/index'
+      end
+
+      private
+
+        # 初期データの取得
+        def initialize_insect_park_data
           insect_park_data = Insect.joins(images: [{ city: :prefecture }, :park, :user])
-          insect_park_data = insect_park_data.where(prefectures: { name: params[:prefecture] }) if params[:prefecture].present?
-          insect_park_data = insect_park_data.where(cities: { name: params[:city] }) if params[:city].present?
 
-          # 採集済み, 未採集の昆虫データ取得処理
-          case params[:status]
-          when 'collected'
-            insect_park_data = insect_park_data.where(images: { user_id: current_user.id })
+          if params[:prefecture].present?
+            insect_park_data = insect_park_data.where(prefectures: { name: params[:prefecture] })
+          end
 
-            @insects = insect_park_data
-                       .select('insects.*, parks.name AS park_name')
-                       .group_by { |insect| [insect.name, insect.sex] }
-                       .map do |(name, sex), insects|
+          if params[:city].present?
+            insect_park_data = insect_park_data.where(cities: { name: params[:city] })
+          end
+
+          insect_park_data
+        end
+
+        # 採集済みの昆虫と公園のリスト
+        def fetch_collected_insects(insect_park_data)
+          collected_insects = insect_park_data.where(images: { user_id: current_user.id })
+
+          collected_insects.select('insects.*, parks.name AS park_name')
+                           .group_by { |insect| [insect.name, insect.sex] }
+                           .map do |(name, sex), insects|
+            {
+              name:,
+              sex:,
+              biological_family: insects.first.biological_family.name,
+              park_name: insects.first.park_name
+            }
+          end
+        end
+
+        # 未採集の昆虫と公園のリスト
+        def fetch_uncollected_insects(insect_park_data)
+          collected_insect_ids = insect_park_data.where(images: { user_id: current_user.id }).select(:insect_id)
+          uncollected_insect_park_data = insect_park_data.where.not(id: collected_insect_ids).distinct
+
+          if params[:lat].present? && params[:lng].present?
+            nearest_parks = Park.near([params[:lat], params[:lng]], 2000, units: :km).order(:distance)
+
+            uncollected_insect_park_data.map do |insect|
+              found_in_park = nearest_parks.detect do |park|
+                park.images.any? { |image| image.insect_id == insect.id }
+              end
+              {
+                name: insect.name,
+                sex: insect.sex,
+                biological_family: insect.biological_family.name,
+                park_name: found_in_park&.name || nil
+              }
+            end
+          else
+            uncollected_insect_park_data
+              .select('insects.*, parks.name AS park_name')
+              .group_by { |insect| [insect.name, insect.sex] }
+              .map do |(name, sex), insects|
               {
                 name:,
                 sex:,
@@ -28,54 +87,16 @@ module Api
                 park_name: insects.first.park_name
               }
             end
-
-          when 'uncollected'
-            # 採集済みの昆虫idを取得
-            collected_insect_ids = insect_park_data.where(images: { user_id: current_user.id }).select(:insect_id)
-            # 採集済みの昆虫データを除外
-            uncollected_insect_park_data = insect_park_data.where.not(id: collected_insect_ids).distinct
-            # 現在位置がある場合は、最寄りのParkを検索して、そのParkの昆虫データを返す
-            @insects = if params[:lat].present? && params[:lng].present?
-                         nearest_parks = Park.near([params[:lat], params[:lng]], 2000, units: :km).order(:distance)
-                         uncollected_insect_park_data.map do |insect|
-                           found_in_park = nearest_parks.detect do |park|
-                             park.images.any? do |image|
-                               image.insect_id == insect.id
-                             end
-                           end
-                           {
-                             name: insect.name,
-                             sex: insect.sex,
-                             biological_family: insect.biological_family.name,
-                             park_name: found_in_park&.name || nil
-                           }
-                         end
-                       else
-                        # 現在位置がない場合,公園名は最初に取得したものを返す
-                         uncollected_insect_park_data
-                           .select('insects.*, parks.name AS park_name')
-                           .group_by { |insect| [insect.name, insect.sex] }
-                           .map do |(name, sex), insects|
-                           {
-                             name:,
-                             sex:,
-                             biological_family: insects.first.biological_family.name,
-                             park_name: insects.first.park_name
-                           }
-                         end
-                       end
-          end
-
-        else
-          # 全ての昆虫データ取得処理
-          all_insects = Insect.all
-          @insects = all_insects.group_by(&:name).map do |name, insects|
-            { name:, available_sexes: insects.map(&:sex) }
           end
         end
 
-        render 'api/v1/insects/index'
-      end
+        # autocomplete用の昆虫のリスト
+        def fetch_all_insects
+          all_insects = Insect.all
+          all_insects.group_by(&:name).map do |name, insects|
+            { name:, available_sexes: insects.map(&:sex) }
+          end
+        end
     end
   end
 end
