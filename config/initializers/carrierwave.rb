4ffe0cfad7882a1d404d12becb552edff1b8c30b
mskk3215 <m.kaaiweb@gmail.com>
2024-02-05 23:17:47 +0900

    feat:DB environment value changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory = ENV.fetch('AWS_BUCKET_FOR_FABLE', nil)
+    config.fog_directory = ENV.fetch('AWS_BUCKET', nil)
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID_FOR_FABLE', nil),
-      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY_FOR_FABLE', nil),
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
       region: 'us-east-1',
       path_style: true
     }
