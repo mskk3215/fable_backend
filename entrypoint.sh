2e3be0c7f0e81a5a6ed11b28fa6ac1cacfd50c23
mskk3215 <m.kaaiweb@gmail.com>
2024-02-05 23:18:49 +0900

    feat:entrypoint.sh changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,28 @@ set -e
 rm -f /backend/tmp/pids/server.pid
 
 # データベースが起動するまで待機する
-until mysqladmin ping -h "db" -u "root" --password="$DB_PASSWORD" &> /dev/null; do
+# until mysqladmin ping -h "db" -u "root" --password="$DB_PASSWORD" &> /dev/null; do
+until mysqladmin ping -h "$DB_HOST" -u "$DB_USERNAME" --password="$DB_PASSWORD" &> /dev/null; do
   echo "Waiting for database to become available..."
   sleep 2
 done
 echo "Database is up!"
 
 # tableを作成
-if ! mysql -h "db" -u "root" --password="$DB_PASSWORD" -e 'use fable_backend_development'; then
+# if ! mysql -h "db" -u "root" --password="$DB_PASSWORD" -e 'use fable_backend_development'; then
+if ! mysql -h "$DB_HOST" -u "$DB_USERNAME" --password="$DB_PASSWORD" -e "use $DB_DATABASE;" ; then
+
   echo "Creating database..."
   rails db:create
 fi
 
 rails db:migrate
 
-if ! rails runner 'User.exists?' ; then
+# if ! rails runner 'User.exists?' ; then
+#   echo "Seeding database..."
+#   rails db:seed
+# fi
+if ! rails runner "exit User.exists? ? 0 : 1"; then
   echo "Seeding database..."
   rails db:seed
 fi
