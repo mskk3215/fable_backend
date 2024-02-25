44814da530b0450c56640af7d4a03d920ea2ec13
mskk3215 <m.kaaiweb@gmail.com>
2024-02-25 12:08:58 +0900

    refactor:comment add for dev environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,9 @@ set -e
 rm -f /backend/tmp/pids/server.pid
 
 # データベースが起動するまで待機する
+# 開発環境
 # until mysqladmin ping -h "db" -u "root" --password="$DB_PASSWORD" &> /dev/null; do
+# 本番環境
 until mysqladmin ping -h "$DB_HOST" -u "$DB_USERNAME" --password="$DB_PASSWORD" &> /dev/null; do
   echo "Waiting for database to become available..."
   sleep 2
@@ -13,7 +15,9 @@ done
 echo "Database is up!"
 
 # tableを作成
+# 開発環境
 # if ! mysql -h "db" -u "root" --password="$DB_PASSWORD" -e 'use fable_backend_development'; then
+# 本番環境
 if ! mysql -h "$DB_HOST" -u "$DB_USERNAME" --password="$DB_PASSWORD" -e "use $DB_DATABASE;" ; then
 
   echo "Creating database..."
@@ -26,6 +30,7 @@ rails db:migrate
 #   echo "Seeding database..."
 #   rails db:seed
 # fi
+# データベースが空の場合、seedを実行
 if ! rails runner "exit User.exists? ? 0 : 1"; then
   echo "Seeding database..."
   rails db:seed
