52575c506c9ccbe6ea433fdd9b012afff4da6bad
mskk3215 <m.kaaiweb@gmail.com>
2024-03-04 23:41:18 +0900

    feat:https listener add to ALB

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -39,33 +39,33 @@ resource "aws_lb_listener_rule" "http_rule_backend" {
   }
 }
 
-# # HTTPSリスナー
-# resource "aws_lb_listener" "alb_listener_https" {
-#   load_balancer_arn = aws_lb.alb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-2016-08"
-#   certificate_arn   = aws_acm_certificate.tokyo_cert.arn # 証明書のARN
-#   # デフォルトのリスナールール
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_tg_frontend.arn
-#   }
-# }
-# # HTTPSリスナールール
-# resource "aws_lb_listener_rule" "https_rule_backend" {
-#   listener_arn = aws_lb_listener.alb_listener_https.arn
-#   priority     = 100
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_tg_backend.arn
-#   }
-#   condition {
-#     path_pattern {
-#       values = ["/api/v1/*"]
-#     }
-#   }
-# }
+# HTTPSリスナー
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.tokyo_cert.arn # 証明書のARN
+  # デフォルトのリスナールール
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg_frontend.arn
+  }
+}
+# HTTPSリスナールール
+resource "aws_lb_listener_rule" "https_rule_backend" {
+  listener_arn = aws_lb_listener.alb_listener_https.arn
+  priority     = 100
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg_backend.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/api/v1/*"]
+    }
+  }
+}
 
 # ----------------------
 # target group
