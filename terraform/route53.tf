33b668bd1e1ee7f074b2fa021eadfb6d4e4ebf08
mskk3215 <m.kaaiweb@gmail.com>
2024-03-15 23:24:17 +0900

    fix:routing for alb modified

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -12,15 +12,11 @@ resource "aws_route53_zone" "route53_zone" {
     Env     = var.environment
   }
 }
-# Aレコード(ALBへのルーティングをマッピング用)
+# # Aレコード(ALBへのルーティングをマッピング用)
 resource "aws_route53_record" "route53_record" {
   zone_id = aws_route53_zone.route53_zone.zone_id
-  name    = "dev-elb.${var.domain}"
-  type    = "A"
-  # AWSリソースへのルーティング
-  alias {
-    name                   = aws_lb.alb.dns_name # ALBのDNS名
-    zone_id                = aws_lb.alb.zone_id  # ALBのZone ID
-    evaluate_target_health = true                # ターゲットのヘルスチェックを有効にするかどうか
-  }
+  name    = "*.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_lb.alb.dns_name]
 }
