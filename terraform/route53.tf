7168c825187b7eb0f88cc10510197a8bf5d803b8
mskk3215 <m.kaaiweb@gmail.com>
2024-03-17 18:35:55 +0900

    fix:route53 record to alb changed from cname to a record

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -12,11 +12,16 @@ resource "aws_route53_zone" "route53_zone" {
     Env     = var.environment
   }
 }
-# # Aレコード(ALBへのルーティングをマッピング用)
-resource "aws_route53_record" "route53_record" {
+
+# Aレコード(ALBへのルーティングをマッピング用)
+resource "aws_route53_record" "route53_a_record_to_alb" {
   zone_id = aws_route53_zone.route53_zone.zone_id
-  name    = "*.${var.domain}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_lb.alb.dns_name]
+  name    = "alb.${var.domain}"
+  type    = "A"
+  # AWSリソースへのルーティング
+  alias {
+    name                   = aws_lb.alb.dns_name # ALBのDNS名
+    zone_id                = aws_lb.alb.zone_id  # CloudFrontのZone ID
+    evaluate_target_health = false               # ターゲットのヘルスチェックを有効にするかどうか
+  }
 }
