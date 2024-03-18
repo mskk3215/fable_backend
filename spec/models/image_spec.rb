b7fc22ee945db896a0c4af6bc2c159f8246c1635
mskk3215 <m.kaaiweb@gmail.com>
2024-03-18 21:59:42 +0900

    refactor:image validation test

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,30 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  before do
-    user = FactoryBot.create(:user)
-    post = FactoryBot.create(:post, user_id: user.id)
-    @image = FactoryBot.build(:image, image_path: 'public/images/test_image.png', user_id: user.id, post_id: post.id)
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post, user:) }
+  let(:image) { FactoryBot.build(:image, image_path: 'public/images/test_image.png', user:, post:) }
 
   describe '画像投稿' do
+    subject { image.valid? }
+
     context '投稿できる場合' do
       it 'imageがあれば投稿できる' do
-        expect(@image).to be_valid
+        expect(subject).to be true
       end
     end
 
     context '投稿できない場合' do
       it 'imageが空では投稿できない' do
-        @image.image = nil
-        @image.valid?
-        expect(@image.errors.full_messages).to include("Image can't be blank")
+        image.image = nil
+        expect(subject).to be false
+        expect(image.errors.full_messages).to include("Image can't be blank")
       end
 
       it '紐づくユーザーが存在しないと投稿できない' do
-        @image.user = nil
-        @image.valid?
-        expect(@image.errors.full_messages).to include('User must exist')
+        image.user = nil
+        expect(subject).to be false
+        expect(image.errors.full_messages).to include('User must exist')
       end
     end
   end
