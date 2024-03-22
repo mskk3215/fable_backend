4047a24e3ccbcbce01d79802c3cef8846cb608f1
mskk3215 <m.kaaiweb@gmail.com>
2024-03-22 21:22:05 +0900

    refactor:post validation test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,23 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before do
-    user = FactoryBot.create(:user)
-    @post = FactoryBot.build(:post, user_id: user.id)
-  end
+  let(:user) { FactoryBot.create(:user) }
 
   describe '投稿機能' do
+    let(:post) { FactoryBot.create(:post, user:) }
+
     context '投稿できる場合' do
       it 'user_idがあれば投稿できる' do
-        expect(@post).to be_valid
+        expect(post).to be_valid
       end
     end
 
     context '投稿できない場合' do
       it '紐づくユーザーが存在しないと投稿できない' do
-        @post.user = nil
-        @post.valid?
-        expect(@post.errors.full_messages).to include('User must exist')
+        post.user = nil
+        post.valid?
+        expect(post.errors.full_messages).to include('User must exist')
       end
     end
   end
