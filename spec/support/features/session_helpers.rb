468d84fd489922884aa163ebaf007e4aebc192e9
mskk3215 <m.kaaiweb@gmail.com>
2024-03-24 10:19:36 +0900

    feat:JSON.parse(response.body) method add

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -2,6 +2,10 @@
 
 module Features
   module SessionHelpers
+    def json
+      JSON.parse(response.body)
+    end
+
     def login(user)
       post api_v1_login_path, params: { session: { email: user.email, password: user.password } }
     end
