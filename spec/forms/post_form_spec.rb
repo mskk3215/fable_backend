2e4f12df212180ae9e67500abec6b32817e3b85e
mskk3215 <m.kaaiweb@gmail.com>
2024-03-24 18:12:38 +0900

    feat:postform test add

---
+++ spec/forms/post_form_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PostForm, type: :model do
+  describe '#save' do
+    let(:user) { create(:user) }
+    let(:image_file) { Rack::Test::UploadedFile.new(Rails.public_path.join('images/test_image.png'), 'image/png') }
+
+    context '有効な画像が与えられた場合' do
+      let(:form) { PostForm.new(current_user: user, images: [image_file]) }
+
+      it '投稿を保存する' do
+        expect { form.save }.to change(Post, :count).by(1).and change(Image, :count).by(1)
+        expect(Post.last.images.count).to eq 1
+      end
+    end
+
+    context '無効な画像データが与えられた場合' do
+      let(:invalid_image_file) { 'invalid data' }
+      let(:form) { PostForm.new(current_user: user, images: [invalid_image_file]) }
+
+      it '投稿を保存しない' do
+        expect { form.save }.not_to change(Post, :count)
+      end
+    end
+  end
+end
