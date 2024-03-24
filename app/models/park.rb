6b59a97e00a93988f8f96a05fd1b50e7468700c3
mskk3215 <m.kaaiweb@gmail.com>
2024-03-24 19:07:32 +0900

    fix:find_or_create_park method modified n park model

--- app/models/park.rb
+++ app/models/park.rb
@@ -21,12 +21,11 @@ class Park < ApplicationRecord
 
   # 公園名と市町村名から公園を検索し、なければ新規で作成する
   def self.find_or_create_park(park_name, city)
-  # 公園名も市町村名もない場合と、公園名がなく市町村名だけの場合はnilを返す
-    return nil if park_name.blank? || city.blank?
+    # 公園名がない場合はnilを返す
+    return nil if park_name.blank?
 
-  # 公園名がDBにある場合、その公園名を返す
+    # 公園名がある場合、公園名をDBから検索、なければ新規作成
     park = Park.find_or_initialize_by(name: park_name, city_id: city.id)
-  # 公園名がDBにない場合、新規でDBに公園名を登録する
     if park.new_record?
       park.prefecture_id = city.prefecture_id
       park.save!
