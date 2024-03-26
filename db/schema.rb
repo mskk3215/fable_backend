164889e1d30660c4a16c09433d9524106ccaaaa0
mskk3215 <m.kaaiweb@gmail.com>
2024-03-26 22:34:27 +0900

    refactor:unneccesary code deleted

--- db/schema.rb
+++ db/schema.rb
@@ -104,7 +104,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_055133) do
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "avatar"
-    t.string "current_location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
