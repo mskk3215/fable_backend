2ee92c7e7b284faa9e434285caf25f2160c5802c
mskk3215 <m.kaaiweb@gmail.com>
2024-03-27 23:10:43 +0900

    feat:session time out setting add

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_fable_backend', domain: '.fable-230918-24b3a9e93ee3.herokuapp.com', same_site: :none, secure: true
+Rails.application.config.session_store :cookie_store, key: '_fable_backend', domain: 'fablesearch.com', same_site: :strict,
+                                                      secure: true, expire_after: 30.minutes
