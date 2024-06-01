cfb8fdd9622074f71ebf5ace68f69cfe4152ccfd
mskk3215 <m.kaaiweb@gmail.com>
2024-06-01 13:01:29 +0900

    fix:timeout limit updated

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.session_store :cookie_store, key: '_fable_backend', domain: 'fablesearch.com', same_site: :strict,
-                                                      secure: true, expire_after: 30.minutes
+                                                      # secure: true, expire_after: 30.minutes
+                                                      secure: true, expire_after: 10.seconds
