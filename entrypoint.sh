1c74617ae8ddd731979aaa2269507eb89a6c9936
mskk3215 <m.kaaiweb@gmail.com>
2024-06-09 16:11:05 +0900

    fix: line to skip the reverse geocode validation callback before executing the db:seed command to prevent addresses from being prefixed with '日本' add

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,11 @@ rails db:migrate
 # データベースが空の場合、seedを実行
 if ! rails runner "exit User.exists? ? 0 : 1"; then
   echo "Seeding database..."
+  # parksのaddressの頭に日本が自動追加されないように一時的にskip
+  rails runner "Park.skip_callback(:validation, :after, :reverse_geocode)"
   rails db:seed
+  rails runner "Park.set_callback(:validation, :after, :reverse_geocode)"
+
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
