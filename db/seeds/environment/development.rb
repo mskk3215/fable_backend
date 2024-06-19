2fc098d2c5ae606070c3216e6a4979c40ad6e8f8
mskk3215 <m.kaaiweb@gmail.com>
2024-06-19 22:50:17 +0900

    fix:db:seed executed to update insect seed data

--- db/seeds/environment/development.rb
+++ db/seeds/environment/development.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require './db/seeds/dummy_data/users'
-require './db/seeds/dummy_data/relationships'
-require './db/seeds/dummy_data/biological_families'
+# require './db/seeds/dummy_data/users'
+# require './db/seeds/dummy_data/relationships'
+# require './db/seeds/dummy_data/biological_families'
 require './db/seeds/dummy_data/insects'
-require './db/seeds/dummy_data/prefecture_cities'
-require './db/seeds/dummy_data/parks'
+# require './db/seeds/dummy_data/prefecture_cities'
+# require './db/seeds/dummy_data/parks'
 require './db/seeds/dummy_data/post_images'
 require './db/seeds/dummy_data/likes'
