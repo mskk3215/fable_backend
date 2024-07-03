de5335e329618884d122258b102db0c6c78cf3ee
mskk3215 <m.kaaiweb@gmail.com>
2024-07-03 21:55:15 +0900

    feat:habitat_place model&migration add

--- app/models/insect.rb
+++ app/models/insect.rb
@@ -9,5 +9,7 @@ class Insect < ApplicationRecord
   has_many :foods, through: :insect_foods
   has_many :insect_tools, dependent: :destroy
   has_many :tools, through: :insect_tools
+
+  belongs_to :habitat_place, dependent: :destroy
   belongs_to :biological_family, dependent: :destroy
 end
