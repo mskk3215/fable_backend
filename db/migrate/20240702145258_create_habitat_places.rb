2c8e94bfe47a975a3b2443bbe13083296e7a4beb
mskk3215 <m.kaaiweb@gmail.com>
2024-07-05 22:47:17 +0900

    fix:habitat_places table modified

--- db/migrate/20240702145258_create_habitat_places.rb
+++ db/migrate/20240702145258_create_habitat_places.rb
@@ -2,8 +2,6 @@ class CreateHabitatPlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :habitat_places do |t|
       t.string :name
-      t.references :insect, null: false, foreign_key: true
-
       t.timestamps
     end
   end
