bc479d31292c8255d77d7edf6eb33728ea64eda8
mskk3215 <m.kaaiweb@gmail.com>
2024-07-10 21:55:59 +0900

    feat:taken_amount_per_month&hour method add in insect model

--- app/models/insect.rb
+++ app/models/insect.rb
@@ -14,4 +14,26 @@ class Insect < ApplicationRecord
 
   belongs_to :habitat_place, dependent: :destroy
   belongs_to :biological_family, dependent: :destroy
+
+  # 各月の撮影枚数を取得するメソッド
+  def taken_amount_per_month
+    (1..12).map do |month|
+      {
+        month:,
+        count: images.where('extract(month from created_at) = ?', month).count
+      }
+    end
+  end
+
+  # 3時間ごとの撮影枚数を取得するメソッド
+  def taken_amount_per_hour
+    (0..7).map do |hour|
+      start_hour = hour * 3
+      end_hour = start_hour + 2
+      {
+        time_slot: "#{start_hour}:00 - #{end_hour}:59",
+        count: images.where('extract(hour from created_at) between ? and ?', start_hour, end_hour).count
+      }
+    end
+  end
 end
