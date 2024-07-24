0145890e4f753da27b9fceba28318908d20baf2b
mskk3215 <m.kaaiweb@gmail.com>
2024-07-24 21:49:47 +0900

    fix:collecte_insect model and associated models updated

--- app/models/insect.rb
+++ app/models/insect.rb
@@ -2,11 +2,10 @@
 
 class Insect < ApplicationRecord
   validates :name, presence: true
-  validates :sex, presence: true
   validates :size, presence: true
   validates :lifespan, presence: true
 
-  has_many :collected_insect_images, dependent: :destroy
+  has_many :collected_insects, dependent: :destroy
   has_many :insect_foods, dependent: :destroy
   has_many :foods, through: :insect_foods
   has_many :insect_tools, dependent: :destroy
@@ -18,9 +17,13 @@ class Insect < ApplicationRecord
   # 各月の撮影枚数を取得するメソッド
   def taken_amount_per_month
     (1..12).map do |month|
+      count = collected_insects.count do |collected_insect|
+        collected_insect &&
+          collected_insect.taken_date_time.month == month
+      end
       {
         month:,
-        count: collected_insect_images.where('extract(month from created_at) = ?', month).count
+        count:
       }
     end
   end
@@ -30,9 +33,13 @@ class Insect < ApplicationRecord
     (0..7).map do |hour|
       start_hour = hour * 3
       end_hour = start_hour + 2
+      count = collected_insects.count do |collected_insect|
+        collected_insect &&
+          (start_hour..end_hour).cover?(collected_insect.taken_date_time.hour)
+      end
       {
         time_slot: "#{start_hour}:00 - #{end_hour}:59",
-        count: collected_insect_images.where('extract(hour from created_at) between ? and ?', start_hour, end_hour).count
+        count:
       }
     end
   end
