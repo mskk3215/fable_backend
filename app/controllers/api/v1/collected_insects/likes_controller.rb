c7a6a6f65a2f06b485d64234534466907fe7b404
mskk3215 <m.kaaiweb@gmail.com>
2024-07-28 01:07:13 +0900

    fix:collected_insect and associated controllers updated

--- app/controllers/api/v1/collected_insects/likes_controller.rb
+++ app/controllers/api/v1/collected_insects/likes_controller.rb
@@ -2,22 +2,22 @@
 
 module Api
   module V1
-    module CollectedInsectImages
+    module CollectedInsects
       class LikesController < ApplicationController
         def create
-          @like = current_user.likes.build(collected_insect_image_id: like_params[:collected_insect_image_id])
+          @like = current_user.likes.build(collected_insect_id: like_params[:collected_insect_id])
           if @like.save
-            render 'api/v1/collected_insect_images/likes/create'
+            render 'api/v1/collected_insects/likes/create'
           else
             render json: { error: ['いいねをすることができませんでした。'] }, status: :unprocessable_entity
           end
         end
 
         def destroy
-          @like = current_user.likes.find_by(collected_insect_image_id: like_params[:collected_insect_image_id])
+          @like = current_user.likes.find_by(collected_insect_id: like_params[:collected_insect_id])
           if @like
             @like.destroy
-            render 'api/v1/collected_insect_images/likes/destroy'
+            render 'api/v1/collected_insects/likes/destroy'
           else
             render json: { error: ['いいねが見つかりません。'] }, status: :not_found
           end
@@ -26,7 +26,7 @@ module Api
         private
 
           def like_params
-            params.permit(:collected_insect_image_id)
+            params.permit(:collected_insect_id)
           end
       end
     end
