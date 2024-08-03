e4984f5b5cbffdd9b61ec57b049522f6b26e63e0
mskk3215 <m.kaaiweb@gmail.com>
2024-08-03 22:15:34 +0900

    feat:models to sighting_notifications add

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many  :posts, dependent: :destroy
   has_many  :collected_insects, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :sighting_notifications, dependent: :destroy
 
   # follow,followedのrelathionships(中間テーブル)との関連付け
   has_many :active_relationships,  class_name:  'Relationship', foreign_key: 'follower_id', dependent: :destroy
