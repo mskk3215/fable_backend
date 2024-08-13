197bfb0b78b57e74f7d2008adbd990994bba7bae
mskk3215 <m.kaaiweb@gmail.com>
2024-08-13 22:21:36 +0900

    fix:api of sighting_notifications modified to fetch unread a notification

--- app/controllers/api/v1/sighting_notifications_controller.rb
+++ app/controllers/api/v1/sighting_notifications_controller.rb
@@ -5,6 +5,9 @@ class Api::V1::SightingNotificationsController < ApplicationController
     @sighting_notifications = if params[:insect_id]
                                 # piturebookページの最近の出没先リストに表示
                                 fetch_recent_collected_insects
+                              elsif params[:icon_button]
+                                # 通知アイコンを未読アイコンにするかどうか
+                                fetch_unread_notifications
                               else
                                 # notificationページでcurrent_userの通知一覧を表示
                                 fetch_current_user_notifications
@@ -12,48 +15,49 @@ class Api::V1::SightingNotificationsController < ApplicationController
     render 'api/v1/sighting_notifications/index'
   end
 
-  def update
-    sighting_notification = SightingNotification.find(params[:id])
-
-    if sighting_notification.update(is_read: true)
-      render json: { status: 'updated' }, status: :ok
-    else
-      render json: { error: [sighting_notification.errors.full_messages] }, status: :unprocessable_entity
-    end
-  end
-
   private
 
     def fetch_recent_collected_insects
       collected_insects = CollectedInsect.where(insect_id: params[:insect_id]).joins(:insect,
                                                                                      :park).order(taken_date_time: :desc).page(params[:page]).limit(5)
-      format_collected_insects(collected_insects)
+      collected_insects.flat_map do |collected_insect|
+        {
+          insect_id: collected_insect.insect_id,
+          insect_name: collected_insect.insect.name,
+          taken_date_time: collected_insect.taken_date_time,
+          park_name: collected_insect.park.name
+        }
+      end
+    end
+
+    def fetch_unread_notifications
+      unread_notifications = current_user.sighting_notifications
+                                         .includes(collected_insect: %i[insect park])
+                                         .where(is_read: false)
+                                         .order(updated_at: :desc)
+                                         .limit(1)
+      format_notifications(unread_notifications)
     end
 
     def fetch_current_user_notifications
-      current_user.sighting_notifications.includes(collected_insect: %i[insect
-                                                                        park]).order(created_at: :desc).page(params[:page]).per(8).map do |notification|
+      notifications = current_user.sighting_notifications
+                                  .includes(collected_insect: %i[insect park])
+                                  .order(updated_at: :desc)
+                                  .page(params[:page])
+                                  .per(8)
+      format_notifications(notifications, mark_as_read: true)
+    end
+
+    # format
+    def format_notifications(notifications, mark_as_read: false)
+      notifications.map do |notification|
+        notification.update(is_read: true) if mark_as_read
         {
-          id: notification.id,
-          collected_insect_id: notification.collected_insect_id,
           insect_id: notification.collected_insect.insect_id,
           insect_name: notification.collected_insect.insect.name,
           taken_date_time: notification.collected_insect.taken_date_time,
           park_name: notification.collected_insect.park&.name,
           is_read: notification.is_read
-        }
-      end
-    end
-
-    # format
-    def format_collected_insects(collected_insects)
-      collected_insects.flat_map do |collected_insect|
-        {
-          collected_insect_id: collected_insect.id,
-          insect_id: collected_insect.insect_id,
-          insect_name: collected_insect.insect.name,
-          taken_date_time: collected_insect.taken_date_time,
-          park_name: collected_insect.park.name
         }
       end
     end
--- spec/requests/api/v1/sighting_notifications_spec.rb
+++ spec/requests/api/v1/sighting_notifications_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::SightingNotifications' do
+RSpec.describe 'Api::V1::SightingNotifications', type: :request do
   let(:user) { create(:user) }
   let(:insect) { create(:insect) }
   let(:park) { create(:park) }
@@ -26,6 +26,23 @@ RSpec.describe 'Api::V1::SightingNotifications' do
       it '期待される通知を返す' do
         expect(json_response).to be_an_instance_of(Array)
         expect(json_response.first['insect_id']).to eq(insect.id)
+                expect(json_response.first['insect_name']).to eq(insect.name)
+        expect(json_response.first['park_name']).to eq(park.name)
+      end
+    end
+
+    context '未読の通知を取得する場合' do
+      before do
+        get api_v1_sighting_notifications_path, params: { icon_button: true }
+      end
+
+      it '成功したレスポンスを返す' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it '未読の通知を返す' do
+        expect(json_response).to be_an_instance_of(Array)
+        expect(json_response.first['is_read']).to be false
       end
     end
 
@@ -40,47 +57,13 @@ RSpec.describe 'Api::V1::SightingNotifications' do
 
       it '期待される通知を返す' do
         expect(json_response).to be_an_instance_of(Array)
-        expect(json_response.first['collected_insect_id']).to eq(collected_insect.id)
         expect(json_response.first['insect_name']).to eq(insect.name)
         expect(json_response.first['park_name']).to eq(park.name)
+        expect(json_response.first['is_read']).to be true
       end
     end
   end
 
-  describe 'PATCH /update' do
-    context '通知を既読としてマークする場合' do
-      before do
-        patch api_v1_sighting_notification_path(sighting_notification.id)
-      end
-
-      it '成功したレスポンスを返す' do
-        expect(response).to have_http_status(:ok)
-      end
-
-      it '通知が既読としてマークされる' do
-        expect(sighting_notification.reload.is_read).to be true
-      end
-    end
-
-    context '更新が失敗する場合' do
-      before do
-        sighting_notification = create(:sighting_notification)
-
-        allow(SightingNotification).to receive(:find).and_return(sighting_notification)
-        allow(sighting_notification).to receive(:update).and_return(false)
-
-        patch api_v1_sighting_notification_path(sighting_notification.id)
-      end
-
-      it '失敗のレスポンスを返す' do
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it 'エラーメッセージを返す' do
-        expect(json_response['error']).to be_present
-      end
-    end
-  end
 end
 
 def json_response
